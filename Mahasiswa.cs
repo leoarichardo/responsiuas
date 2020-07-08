d6af0da74d884016bd4ac677e1ff00fc4d5e4650
leoarichardo <leo.richardo@students.amikom.ac.id>
2020-07-08 12:34:28 +0700

    first commit

---
+++ Mahasiswa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+        public string nim { get; set; }
+        public string nama_mahasiswa { get; set; }
+        public char jenis_kelamin { get; set; }
+        public double ipk  { get; set; }
+    }
+}
